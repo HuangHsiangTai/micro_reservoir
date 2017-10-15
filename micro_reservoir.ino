17d695d4433b0aec36861ab8e70b041ee3e123e0
Shawn Huang <HuangHsiangTai@gmail.com>
2017-10-15 08:23:14 +0800

    remove Light Sensor, add water threshold, not verified

--- micro_reservoir.ino
+++ micro_reservoir.ino
@@ -1,51 +1,33 @@
 #include <CircularBuffer.h>
-int LIGHT_UNKNOWN_STATE = 0;
-int LIGHT_ACTIVE_STATE = 1;
-int LIGHT_INACTIVE_STATE = 2;
-int WATER_DETECTION_MS_IN_UNKNOWN_STATE = 4000;
-int WATER_DETECTION_MS_IN_INACTIVE_STATE = 4000;
-int WATER_DETECTION_MS_IN_ACTIVE_STATE = 1000;
-int LIGHT_LENGTH = 6;
-CircularBuffer<int,LIGHT_LENGTH>lightList;
-boolean isHigh = false;
-int onlineTest= true;
-int D3 = 3; 
+int WATER_THRESHOlD = 1200;
+int WATER_DETECTION_MS = 5000;
+boolean enableRelay = false;
+int D3 = 3;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(D3, OUTPUT);
 }
 
-int averageLight(){
-  int i;
-  int out=0;
-  for(i=0; i<LIGHT_LENGTH; i++){
-    out = out + lightList[i];
-  }
-  return out/LIGHT_LENGTH;
-}
-
 void loop() {
   // put your main code here, to run repeatedly:
   // read the input on analog pin 0:
-  int lightSensor = analogRead(A0);
-  // print out the value you read:
-  Serial.print("light sensor value=");
-  Serial.print(lightSensor);
-  Serial.print(",average light value=");
-  int aveLight = averageLight();
-  Serial.println(aveLight);
-  lightList.push(lightSensor);
+  //int lightSensor = analogRead(A0);
   int waterSensor = analogRead(A1);
    Serial.print("water sensor value=");
    Serial.println(waterSensor);
+   if(waterSensor >= WATER_THRESHOlD){
+     enableRelay= false;
+   } else {
+     enableRelay = true;
+   }
   //Serial.println("water sensor value=%d", waterSensor);
   if (isHigh){
     digitalWrite(D3, HIGH);
-    isHigh = false;  
+    isHigh = false;
   } else {
     digitalWrite(D3, LOW);
-    isHigh = true;  
+    isHigh = true;
   }
-  delay(3000); 
+  delay(WATER_DETECTION_MS);
 }
