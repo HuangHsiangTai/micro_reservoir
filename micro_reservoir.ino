f7404c5d7f8bfb06963808b515f607fa676988d0
HuangHsiangTai <HuangHsiangTai@gmail.com>
2017-10-17 12:46:30 +0800

    modify detection time and add state

--- micro_reservoir.ino
+++ micro_reservoir.ino
@@ -1,8 +1,15 @@
 #include <CircularBuffer.h>
 int WATER_THRESHOlD = 1200;
-int WATER_DETECTION_MS = 5000;
+int WATER_STANDBY_DETECTION_MS = 3000;
+int WATER_ACTIVE_DETECTION_MS = 500;
+int MAX_ADD_WATER_TIME = 10000;
+int STAND_BY_STATE = 0;
+int ADD_WATER_STATE = 1;
+
 boolean enableRelay = false;
+int detectionTime = WATER_STANDBY_DETECTION_MS; 
 int D3 = 3;
+int state = STAND_BY_STATE;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -13,21 +20,18 @@ void loop() {
   // put your main code here, to run repeatedly:
   // read the input on analog pin 0:
   //int lightSensor = analogRead(A0);
+  //TODO: check time out 
   int waterSensor = analogRead(A1);
    Serial.print("water sensor value=");
    Serial.println(waterSensor);
    if(waterSensor >= WATER_THRESHOlD){
      enableRelay= false;
+     detectionTime = WATER_STANDBY_DETECTION_MS;
+     digitalWrite(D3, HIGH);
    } else {
      enableRelay = true;
+     detectionTime = WATER_ACTIVE_DETECTION_MS;
+     digitalWrite(D3, LOW);
    }
-  //Serial.println("water sensor value=%d", waterSensor);
-  if (isHigh){
-    digitalWrite(D3, HIGH);
-    isHigh = false;
-  } else {
-    digitalWrite(D3, LOW);
-    isHigh = true;
-  }
-  delay(WATER_DETECTION_MS);
+  delay(detectionTime);
 }
