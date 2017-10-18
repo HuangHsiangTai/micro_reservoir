bc9cc26442c8d169aa6857f49d44b901e156aab6
Shawn Huang <HuangHsiangTai@gmail.com>
2017-10-18 22:33:55 +0800

    add time constraint, not testing yet

--- micro_reservoir.ino
+++ micro_reservoir.ino
@@ -7,9 +7,10 @@ int STAND_BY_STATE = 0;
 int ADD_WATER_STATE = 1;
 
 boolean enableRelay = false;
-int detectionTime = WATER_STANDBY_DETECTION_MS; 
+int detectionTime = WATER_STANDBY_DETECTION_MS;
 int D3 = 3;
 int state = STAND_BY_STATE;
+int addWaterTime = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -20,18 +21,34 @@ void loop() {
   // put your main code here, to run repeatedly:
   // read the input on analog pin 0:
   //int lightSensor = analogRead(A0);
-  //TODO: check time out 
+  //TODO: check time out
   int waterSensor = analogRead(A1);
    Serial.print("water sensor value=");
    Serial.println(waterSensor);
    if(waterSensor >= WATER_THRESHOlD){
-     enableRelay= false;
+     if(enableRelay){
+       digitalWrite(D3, LOW);
+       Serial.println("stop adding water");
+       enableRelay= false;
+     }
+     Serial.println("water level>threshold");
      detectionTime = WATER_STANDBY_DETECTION_MS;
-     digitalWrite(D3, HIGH);
+     addWaterTime = 0;
    } else {
-     enableRelay = true;
      detectionTime = WATER_ACTIVE_DETECTION_MS;
-     digitalWrite(D3, LOW);
+     addWaterTime = addWaterTime + detectionTime;
+     if(addWaterTime>=MAX_ADD_WATER_TIME) {
+       enableRelay= false;
+       digitalWrite(D3, LOW);
+       Serial.println("add water too much. stop adding water");
+     }else {
+       if(!enableRelay){
+         enableRelay= true;
+         digitalWrite(D3, HIGH);
+         Serial.println("start adding water");
+       }
+
+     }
    }
   delay(detectionTime);
 }
